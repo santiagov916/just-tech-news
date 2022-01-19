a5adf0ac9613d696f28ab71be60908b61449aec0
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-01-18 17:33:23 -0800

    added post routes and server connection

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes.js');
 
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+
+router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
 
 
--- models/index.js
+++ models/index.js
@@ -2,5 +2,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { User };
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, Post };
